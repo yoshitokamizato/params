20c4b4aa87b5eca347875475cdd168136365a7ad
yuri kurokawa <mp963@ezweb.ne.jp>
2019-01-15 21:27:10 +0900

    chartjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,40 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require Chart.min
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+window.addEventListener('DOMContentLoaded', function(){
+  var ctx = document.getElementById("myChart").getContext('2d')
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: gon.labels,
+      datasets: [{
+        label: 'Score',
+        data: gon.scores,
+        backgroundColor: [
+          'rgba(255, 99, 135, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 135, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 3
+      }]
+    }
+  });
+});

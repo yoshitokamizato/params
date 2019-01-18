e38557b37d493d9ecdf5ca3223b2a78b5756933e
yuri kurokawa <mp963@ezweb.ne.jp>
2019-01-18 22:29:59 +0900

    add_scores

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def index
     @message = "Hello World!!"
     gon.labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+    gon.scores = [1, 2, 3, 4, 5, 6]
   end
 end
